54df3c744e2c15b2a04010b1beb8e4455e25c337
sifiso masaleguba <sifisomasalegub@gmail.com>
2023-10-01 20:14:17 +0200

    added new project for react and net core testing

---
+++ TestEmployment/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEmployment.Models
+{
+    public class Department
+    {
+        public int Department_ID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ TestEmployment/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEmployment.Models
+{
+    public class Employee
+    {
+        public int Employee_ID { get; set; }
+        public string EmployeeName { get; set; }
+        public int Department_ID { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
